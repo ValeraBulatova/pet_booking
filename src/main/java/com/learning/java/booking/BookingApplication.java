250b7eba78ddc1694c19061116272ed270d1e072
vbulatova <vbulatova@mediatech-cx.com>
2021-07-11 14:12:01 +0300

    initial commit helloWorld

---
+++ src/main/java/com/learning/java/booking/BookingApplication.java
@@ -0,0 +1,24 @@
+package com.learning.java.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApplication.class, args);
+	}
+
+//	open http://localhost:8080/valera to see the result
+
+	@GetMapping("/valera")
+	public String hello(@RequestParam(value = "name", defaultValue = "Valera") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
